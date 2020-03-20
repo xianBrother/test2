c375c736c5a7833043a80583c51653a9e4f9ee71
xianBrother <1036707498@qq.com>
2020-03-20 15:42:31 +0800

    nnnn

--- app/src/main/java/com/baoze/wifi/MainActivity.java
+++ app/src/main/java/com/baoze/wifi/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("hahah","test");
+
+        Log.d("hhhh","jjj");
     }
 }
